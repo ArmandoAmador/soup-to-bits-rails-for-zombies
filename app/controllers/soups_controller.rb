2be90a2f6087de0ec74736369c7687231327a5eb
Armando Amador <arm.amador@gmail.com>
2015-03-02 23:54:45 -0500

    Add index and show actions to Soups controller
     - Add show view to soups
     - Add json format to index and show

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,2 +1,16 @@
 class SoupsController < ApplicationController
+  def index
+    @soups = Soup.all
+    respond_to do |format|
+      format.json { render json: @soups }
+    end
+  end
+
+  def show
+    @soup = Soup.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @soup }
+    end
+  end
 end
