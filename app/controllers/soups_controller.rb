b01e8cacef6fbc047ceebe248af8752e0fef47f7
Armando Amador <arm.amador@gmail.com>
2015-03-03 00:21:25 -0500

    Add toggle featured action to soups
     - toggle will let us change a soup from featured to not featured
     - redirects back to itself
     - Create root page for app that routes to categories index
     - all categories routes to categories root page
     - add flash[:notice] for when you toggle a soup
     - Add flash messages to application.html.erb

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,4 +1,6 @@
 class SoupsController < ApplicationController
+  before_action :fetch_soup, only: [:show, :edit, :update, :destroy, :toggle_featured]
+
   def index
     @soups = Soup.all
     respond_to do |format|
@@ -7,10 +9,33 @@ class SoupsController < ApplicationController
   end
 
   def show
-    @soup = Soup.find(params[:id])
     respond_to do |format|
       format.html
       format.json { render json: @soup }
     end
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def toggle_featured
+    @soup.toggle!(:featured)
+    flash[:notice] = "Successfully changed the featured flag."
+    redirect_to @soup
+  end
+
+  private
+
+  def fetch_soup
+    @soup = Soup.find(params[:id])
   end
 end
